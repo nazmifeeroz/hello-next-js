9ca4dcf0cad3ceed3cd779446dbf7b54f632943e
Nazmi <nazmifeeroz@hotmail.com>
2018-12-14 19:14:08 +0800

    clean up with gitignore

---
+++ pages/post.js
@@ -0,0 +1,9 @@
+import { withRouter } from "next/router";
+import Layout from "../lib/layout.js";
+
+export default withRouter(props => (
+  <Layout>
+    <h1>{props.router.query.title}</h1>
+    <p>This is the blog post content.</p>
+  </Layout>
+));
