d8f0e80f966df23bd1f8c60eb72ff9588aa22006
Nazmi <nazmifeeroz@hotmail.com>
2018-12-15 18:11:10 +0800

    learn bout styling jsx

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,58 @@ import Layout from "../components/layout";
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 
+const getPosts = () => {
+  return [
+    { id: "hello-nextjs", title: "Hello Next.js" },
+    { id: "learn-nextjs", title: "Learn Next.js is awesome" },
+    { id: "deploy-nextjs", title: "Deploy Next.js" }
+  ];
+};
+
+const PostLink = ({ post }) => (
+  <li key={post.id}>
+    <Link as={`/p/${post.id}`} href={`/post?id=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+);
+
 const Index = props => (
-    <Layout>
-      <h1>Batman TV Shows</h1>
-      <ul>
-        {props.shows.map(({ show }) => (
-          <li key={show.id}>
-            <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Layout>
-  );
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      {getPosts().map(post => (
+        <PostLink key={post.id} post={post} />
+      ))}
+    </ul>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
+  </Layout>
+);
 
 Index.getInitialProps = async function() {
   console.log("here");
