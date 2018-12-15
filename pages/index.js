6dbf6aa384bd40af80fb89ddbe07e212edc2f6a1
Nazmi <nazmifeeroz@hotmail.com>
2018-12-15 10:31:32 +0800

    wip: props.shows not working

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
 import Layout from "../components/layout";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-const PostLink = props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
+const Index = props => {
+  console.log("props", props);
+  return (
+    <Layout>
+      <h1>Batman TV Shows</h1>
+      <ul>
+        {props.shows.map(({ show }) => (
+          <li key={show.id}>
+            <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
 
-export default () => (
-  <Layout>
-    <h1>My Blog</h1>
-    <ul>
-      <PostLink id="hello" title="Hello Next.js" />
-      <PostLink id="learn" title="Learn Next.js is awesome" />
-      <PostLink id="deploy" title="Deploy apps with Zeit" />
-    </ul>
-  </Layout>
-);
+Index.getInitialProps = async function() {
+  console.log("here");
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json;
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data
+  };
+};
+
+export default Index;
